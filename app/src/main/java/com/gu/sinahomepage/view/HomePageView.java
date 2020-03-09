d45b0b9bf6774cf9f909310ddd753dd3445c3091
gu <gu@gudeMacBook-Pro.local>
2020-03-10 02:52:16 +0800

    init

---
+++ app/src/main/java/com/gu/sinahomepage/view/HomePageView.java
@@ -0,0 +1,18 @@
+package com.gu.sinahomepage.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class HomePageView extends FrameLayout {
+  public HomePageView(@NonNull Context context) {
+    super(context);
+  }
+
+  public HomePageView(@NonNull Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+}
